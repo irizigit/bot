b219ec912937c03a1671d9310b36a33240b7baf1
irizigit <iriziirizi13@gmail.com>
2026-02-15 21:33:27 +0100

    Update OWNER_ID in index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ let requestCount = 0;
 let isBotReady = false;
 const PDF_ARCHIVE_GROUP = '120363403563982270@g.us';
 const IMAGES_ARCHIVE_GROUP = '120363400468776166@g.us';
-const OWNER_ID = '84564227018@c.us';
+const OWNER_ID = '2126219577758@c.us';
 const PROTECTION_PASSWORD = process.env.BOT_PASSWORD || 'your_secure_password';
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY || 'AIzaSyAtjzws4mfUHl3LkNuXUTtwubSBTmGSsc8';
 
